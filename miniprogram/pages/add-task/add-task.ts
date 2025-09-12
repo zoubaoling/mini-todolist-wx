0e75314eb99fb5cf4963f253405f317f07325041
zoubaoling <izoubaoling@163.com>
2025-09-13 00:26:56 +0800

    feat: 1. 编写添加任务页面数据逻辑处理 2. 引入dayjs

--- miniprogram/pages/add-task/add-task.ts
+++ miniprogram/pages/add-task/add-task.ts
@@ -1,40 +1,57 @@
 import { TASK_PRIORITY, TASK_CATEGORY } from "../../constants/index"
+import { debounce } from "../../utils/util"
+import TimeUtils from "../../utils/timer"
+import * as serverApi from '../../server/index'
 Page({
   data: {
     priorityList: [] as Array<{type: string, desc: string, label: string, class: string}>,
-    selectedCategory: '',
-    selectedDate: '',
     selectedTime: '',
-    isReminderEnabled: false
+    taskFormData: {
+      text: '',
+      desc: '',
+      category: 'WORK',
+      isReminder: false,
+      // 截止日期
+      // picker: mode=time时，格式为 "YYYY-MM-DD"
+      deadlineDate: TimeUtils.format(new Date(), 'YYYY-MM-DD'),
+      // picker: mode=time时，格式为 hh:mm
+      deadlineTime: TimeUtils.format(new Date(), 'hh:mm'),
+      priority: 'HIGH',
+      status: 'DOING'
+    }
   },
   onLoad() {
+    this.debounceUpdateFormFieldData = debounce(this.updateFormFieldData.bind(this), 500)
+     // 确保数据初始化
     this.initPriorityList()
     this.initCategoryList()
   },
   handleBack() {
     wx.navigateBack()
   },
-  handleSaveTask() {
-    console.log('save task')
-  },
-  onDateChange(e: any) {
-    this.setData({
-      selectedDate: e.detail.value
+  async handleSaveTask() {
+    const deadline = TimeUtils.combineDateTimeSafe(this.data.taskFormData.deadlineDate, this.data.taskFormData.deadlineTime)
+    await taskApi.addTask({
+      ...this.data.taskFormData,
+      deadline
     })
+    wx.navigateBack()
   },
-  onTimeChange(e: any) {
+  updateFormFieldData (e: any) {
+    const { field, type } = e.currentTarget.dataset
     this.setData({
-      selectedTime: e.detail.value
-    })
-  },
-  onReminderToggle(e: any) {
-    this.setData({
-      isReminderEnabled: e.detail.value
+      [`taskFormData.${field}`]: ['text', 'desc', 'isReminder'].includes(field) ? e.detail.value : type
     })
   },
   initPriorityList () {
     const list = Object.values(TASK_PRIORITY)
-    .map(({type, label, desc}) => ({type, desc, label: `${label}优先级`, class: `priority-${type.toLowerCase()}`}))
+    .map(({type, label, desc}) => ({
+      type,
+      desc,
+      label: `${label}优先级`,
+      class: `priority-${type.toLowerCase()}`,
+      selectedClass: `priority-${type.toLowerCase()}-selected`
+    }))
     this.setData({
       priorityList: list
     })
@@ -42,8 +59,7 @@ Page({
   initCategoryList () {
     const list = Object.values(TASK_CATEGORY)
     this.setData({
-      categoryList: list,
-      selectedCategory: list.length ? list[0].type : ''
+      categoryList: list
     })
   }
 })
